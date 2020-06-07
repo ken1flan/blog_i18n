5140c00f2b77b3e2275e2a1236d716457c0111b6
ken1flan <ken1flan@gmail.com>
2020-06-07 14:38:10 +0900

    Generate scaffold user

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_06_07_053431) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "login_id"
+    t.string "password_digest"
+    t.string "name"
+    t.string "email"
+    t.string "phone"
+    t.text "introduction"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
