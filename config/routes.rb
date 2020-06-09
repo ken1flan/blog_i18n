98fbefaef34edf5017544449b17ffffae607e4cb
ken1flan <ken1flan@gmail.com>
2020-06-10 08:55:23 +0900

    Generate scaffold blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :blogs
   resources :users
   resource :session, only: %w[new create destroy]
   root 'top#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_07_053431) do
+ActiveRecord::Schema.define(version: 2020_06_09_234727) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "login_id", null: false
@@ -26,4 +35,5 @@ ActiveRecord::Schema.define(version: 2020_06_07_053431) do
     t.index ["phone"], name: "index_users_on_phone", unique: true
   end
 
+  add_foreign_key "blogs", "users"
 end
