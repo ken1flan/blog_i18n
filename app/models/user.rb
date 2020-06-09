655dab67c823b88573c94ffa4e65a18ec3993d21
ken1flan <ken1flan@gmail.com>
2020-06-09 09:45:09 +0900

    Add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :login_id, presence: true, length: { minimum: 5, maximum: 32 }, uniqueness: true
+  validates :name, presence: true, length: { minimum: 5, maximum: 255 }
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: true
+  validates :phone, presence: true, length: { minimum: 10, maximum: 11 }, uniqueness: true
+  validates :introduction, length: { maximum: 10_000 }
 end
