f448af7fdee0c668cea738a0af5f25cb41dd81ec
ken1flan <ken1flan@gmail.com>
2020-06-11 08:08:19 +0900

    Fix migration file

--- db/migrate/20200610225148_create_user_registrations.rb
+++ db/migrate/20200610225148_create_user_registrations.rb
@@ -1,10 +1,10 @@
 class CreateUserRegistrations < ActiveRecord::Migration[6.0]
   def change
     create_table :user_registrations do |t|
-      t.string :email
-      t.string :token
-      t.references :user, null: false, foreign_key: true
-      t.datetime :expired_at
+      t.string :email, null: false
+      t.string :token, null: false
+      t.references :user, foreign_key: true
+      t.datetime :expired_at, null: false
 
       t.timestamps
     end
