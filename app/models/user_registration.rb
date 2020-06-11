b16b55fe7ebd7a5b49644f8a20adf736fc7b6602
ken1flan <ken1flan@gmail.com>
2020-06-11 09:07:43 +0900

    Add token and expired_at

--- app/models/user_registration.rb
+++ app/models/user_registration.rb
@@ -1,3 +1,19 @@
 class UserRegistration < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
+  before_create :set_token, :set_expired_at
+
+  EXPIRATION_PERIOD = 24.hours
+
+  private
+
+    def set_token
+      loop do
+        self.token = SecureRandom.urlsafe_base64
+        break if self.class.find_by(token: token).blank?
+      end
+    end
+
+    def set_expired_at
+      self.expired_at ||= EXPIRATION_PERIOD.since
+    end
 end
