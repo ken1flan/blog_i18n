9c251afbbd609eb9cddd279a9597f75a85c4eebc
ken1flan <ken1flan@gmail.com>
2020-06-11 22:15:56 +0900

    Add #welcome_email

--- app/mailers/user_registration_mailer.rb
+++ app/mailers/user_registration_mailer.rb
@@ -1,2 +1,6 @@
 class UserRegistrationMailer < ApplicationMailer
+  def welcome_email
+    @user_registration = params[:user_registration]
+    mail(to: @user_registration.email, subject: 'Welcome to blog i18n sample!')
+  end
 end
