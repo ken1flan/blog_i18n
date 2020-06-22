4b337bd1ff5d46df3affff57010c1f6498a8921a
ken1flan <ken1flan@gmail.com>
2020-06-23 08:52:34 +0900

    Add blog locale

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,4 +4,8 @@ class Blog < ApplicationRecord
   validates :title, presence: true, length: { maximum: 64 }
   validates :body, presence: true, length: { maximum: 10_000 }
   validates :user_id, presence: true
+
+  def author?(user)
+    self.user == user
+  end
 end
