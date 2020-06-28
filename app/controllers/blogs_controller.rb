560994917ac3141a9ce3c7ae23aa6ced61cf20fc
ken1flan <ken1flan@gmail.com>
2020-06-28 10:11:44 +0900

    Fix created_at

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,7 +5,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs = Blog.order(created_at: :desc)
   end
 
   # GET /blogs/1
